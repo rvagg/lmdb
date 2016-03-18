b0f0ac2ac03e3cfcb71d1cd0c9fb948446e0111d
Christopher Jeffrey <chjjeffrey@gmail.com>
2016-03-18 15:11:24 -0700

    use nan try catches and fatalexceptions.

--- src/database.cc
+++ src/database.cc
@@ -424,10 +424,10 @@ NAN_METHOD(Database::Close) {
           v8::Local<v8::Value> argv[] = {
             Nan::New<v8::FunctionTemplate>()->GetFunction() // empty callback
           };
-          v8::TryCatch try_catch;
+          Nan::TryCatch try_catch;
           Nan::Callback(end).Call(iterator->handle(), 1, argv);
           if (try_catch.HasCaught()) {
-            node::FatalException(try_catch);
+            Nan::FatalException(try_catch);
           }
         }
     }
@@ -562,14 +562,14 @@ NAN_METHOD(Database::Iterator) {
   // each iterator gets a unique id for this Database, so we can
   // easily store & lookup on our `iterators` map
   uint32_t id = database->currentIteratorId++;
-  v8::TryCatch try_catch;
+  Nan::TryCatch try_catch;
   v8::Handle<v8::Object> iteratorHandle = Iterator::NewInstance(
       info.This()
     , Nan::New<v8::Number>(id)
     , optionsObj
   );
   if (try_catch.HasCaught()) {
-    node::FatalException(try_catch);
+    Nan::FatalException(try_catch);
   }
 
   nlmdb::Iterator *iterator =
